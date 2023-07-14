637a0bda8962b117597cf974720d68a0ae8ecfdc
mwar8205 <mwar8205@gmail.com>
2023-07-14 23:18:06 +0000

    first commit

---
+++ dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (
+  message varchar(255) NOT NULL
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  INSERT INTO test(message) VALUES('Adinusa - managing-handlers');
+  INSERT INTO test(message) VALUES('username');
+  INSERT INTO test(message) VALUES('Ansible is fun')
