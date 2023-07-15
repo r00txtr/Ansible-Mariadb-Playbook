d0c1c24e6987e69526d22680e2bc1079b1629520
r00txtr <mwar8205@gmail.com>
2023-07-15 08:20:56 +0700

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS test (
   message varchar(255) NOT NULL
   ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
   INSERT INTO test(message) VALUES('Adinusa - managing-handlers');
-  INSERT INTO test(message) VALUES('username');
+  INSERT INTO test(message) VALUES('r00txtr1');
   INSERT INTO test(message) VALUES('Ansible is fun')
